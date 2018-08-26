cb66b332f2948203561e058048572217bfed706d
insighthorizon <majiricek@seznam.cz>
2018-08-26 13:14:47 +0200

    print 0xff & *0x21

--- test1/main.c
+++ test1/main.c
@@ -6,15 +6,14 @@
 int main(void) {
   WDTCTL = WDTPW | WDTHOLD; // stop watchdog timer
 
-  P1DIR = (0x01 << 6) | (0x01); // setup pin 1 and pin 6 in port 1 as output
+  P1DIR = (P1DIR & 0xff00) + (((0x01 << 6) | (0x01)) & 0x00ff); // setup pin 1 and pin 6 in port 1 as output
   
-  P1OUT =  0x01; // set pin 1 to log. one
+  P1OUT = (P1OUT & 0xff00) + (0x01 & 0x00ff); // set pin 1 to log. one
 
 
   for(;;)
     {
-      P1OUT ^= 0x01; // toggle pin 1
-      P1OUT ^= 0x01 << 6; // toggle pin 6
+      P1OUT ^= 0x01 | (0x01 << 6); // toggle pin 1 and toggle pin 6
 
       for(int i = 0; i < 32767; i++);
     }
